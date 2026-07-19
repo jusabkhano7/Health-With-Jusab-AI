dc3693afeaf8e3d0826bf9c374d490aa51765912
jusabkhano7 <jusabkhan78615@gmail.com>
2026-07-19 14:19:27 +0530

    Added Generate Content functionality

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function generateContent() {
-  alert("AI Content Generation Started!");
+
+let topic = document.getElementById("topic").value;
+
+if(topic===""){
+alert("Please enter a topic.");
+return;
+}
+
+document.getElementById("output").innerHTML =
+"<h3>Generated Content</h3><p>You selected the topic: <b>" +
+topic +
+"</b></p>";
+
 }
